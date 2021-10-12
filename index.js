deb119876a81421e547113215a7c0d70d76160b7
Lukaa <86567353+4Juice@users.noreply.github.com>
2021-10-12 14:34:09 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Counter from "./components/counter";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
